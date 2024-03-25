24131324b6fac6bf78a32ac7e7a77ede1eff094b
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-03-25 13:33:17 -0300

    Internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
-
-import 'src/investment_manager_app.dart';
+import 'src/l10n/l10n.dart';
 
 void main() {
   runApp(const InvestmentManagerApp());
+}
+
+class InvestmentManagerApp extends StatelessWidget {
+  const InvestmentManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Investment Manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Scaffold(
+        body: Text(AppLocalizations.of(context)!.test),
+      ),
+    );
+  }
 }
