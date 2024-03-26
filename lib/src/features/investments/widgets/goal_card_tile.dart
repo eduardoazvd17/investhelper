d0bec05b0f15958c32a06cd80a9670f01b9c0ba4
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-03-26 14:42:46 -0300

    Creating goal tile

---
+++ lib/src/features/investments/widgets/goal_card_tile.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GoalCardTile extends StatefulWidget {
+  const GoalCardTile({super.key});
+
+  @override
+  State<GoalCardTile> createState() => _GoalCardTileState();
+}
+
+class _GoalCardTileState extends State<GoalCardTile> {
+  late final ScrollController _textScrollController;
+
+  @override
+  void initState() {
+    _textScrollController = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textScrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 225,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 2.5),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Scrollbar(
+                    controller: _textScrollController,
+                    thumbVisibility: true,
+                    child: SingleChildScrollView(
+                      controller: _textScrollController,
+                      child: const Text(
+                        'Aqui ficará a descrição da sua meta.',
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: LinearProgressIndicator(value: 0.75),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/investments/widgets/investments_resume_card.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart' show CupertinoIcons;
+import 'package:flutter/material.dart';
+
+import '../../../l10n/l10n.dart';
+
+class InvestmentsResumeCard extends StatelessWidget {
+  const InvestmentsResumeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Text(
+                    AppLocalizations.of(context)!.totalInvestment,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  visualDensity: VisualDensity.compact,
+                  icon: const Icon(CupertinoIcons.eye_slash),
+                ),
+              ],
+            ),
+            Text(
+              'R\$ 0,00',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(color: Colors.green),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 10),
+              child: Text(
+                AppLocalizations.of(context)!.monthsOperations,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            _movimentationTile(
+              context: context,
+              tooltip: AppLocalizations.of(context)!.buyOperations,
+              icon: CupertinoIcons.arrow_up_right,
+              value: 'R\$ 0,00',
+              color: Colors.green,
+            ),
+            const SizedBox(height: 5),
+            _movimentationTile(
+              context: context,
+              tooltip: AppLocalizations.of(context)!.sellOperations,
+              icon: CupertinoIcons.arrow_down_left,
+              value: 'R\$ 0,00',
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _movimentationTile({
+    required BuildContext context,
+    required String tooltip,
+    required IconData icon,
+    required String value,
+    required Color color,
+  }) {
+    return Tooltip(
+      message: tooltip,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            icon,
+            size: 30,
+            color: color,
+          ),
+          Text(
+            value,
+            style:
+                Theme.of(context).textTheme.titleLarge?.copyWith(color: color),
+          ),
+        ],
+      ),
+    );
+  }
+}
