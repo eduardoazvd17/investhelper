e92375ade718fd9c0f675cb840a2dc5830c96099
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-03-25 23:42:49 -0300

    Refactoring, improving code

--- lib/src/core/utils/app_theme.dart
+++ lib/src/core/utils/app_theme.dart
@@ -8,6 +8,16 @@ class AppTheme {
   static const _kDarkBackgroundColor = Colors.black;
 
   static ThemeData get lightTheme => ThemeData(
+        useMaterial3: true,
+        primaryColor: _kPrimaryColor[200],
+        scaffoldBackgroundColor: _kLightBackgroundColor,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: _kPrimaryColor,
+          brightness: Brightness.light,
+          surface: _kLightBackgroundColor,
+          surfaceTint: _kLightBackgroundColor,
+          secondary: Colors.blueGrey,
+        ),
         appBarTheme: const AppBarTheme(
           centerTitle: _kCenterTitle,
           backgroundColor: _kLightBackgroundColor,
@@ -28,18 +38,24 @@ class AppTheme {
             ),
           ),
         ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: _kPrimaryColor,
-          brightness: Brightness.light,
-          surface: _kLightBackgroundColor,
-          surfaceTint: _kLightBackgroundColor,
-          secondary: Colors.blueGrey,
+        dialogTheme: DialogTheme(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(_kBorderRadius),
+          ),
         ),
-        scaffoldBackgroundColor: _kLightBackgroundColor,
-        useMaterial3: true,
       );
 
   static ThemeData get darkTheme => ThemeData(
+        useMaterial3: true,
+        primaryColor: _kPrimaryColor[200],
+        scaffoldBackgroundColor: _kDarkBackgroundColor,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: _kPrimaryColor,
+          brightness: Brightness.dark,
+          surface: Colors.transparent,
+          surfaceTint: Colors.transparent,
+          secondary: Colors.blueGrey,
+        ),
         appBarTheme: const AppBarTheme(
           centerTitle: _kCenterTitle,
           backgroundColor: _kDarkBackgroundColor,
@@ -60,15 +76,10 @@ class AppTheme {
             ),
           ),
         ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: _kPrimaryColor,
-          brightness: Brightness.dark,
-          surface: Colors.transparent,
-          surfaceTint: Colors.transparent,
-          secondary: Colors.blueGrey,
+        dialogTheme: DialogTheme(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(_kBorderRadius),
+          ),
         ),
-        primaryColor: _kPrimaryColor[200],
-        scaffoldBackgroundColor: _kDarkBackgroundColor,
-        useMaterial3: true,
       );
 }
