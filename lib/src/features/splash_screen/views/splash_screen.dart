dfb30c30dbf16c28d193726213399fdc90cfcc6e
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-03-28 08:56:52 -0300

    Adding logo to splash

--- lib/src/features/splash_screen/views/splash_screen.dart
+++ lib/src/features/splash_screen/views/splash_screen.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 
 import '../../../core/utils/app_theme.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'InvestHelper',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
+      home: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          height: 180,
         ),
       ),
     );
