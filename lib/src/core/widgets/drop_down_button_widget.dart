9787da29e3326dc76c808785bb6d341867444b4d
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-03-28 10:54:20 -0300

    Adding title to drop down

--- lib/src/core/widgets/drop_down_button_widget.dart
+++ lib/src/core/widgets/drop_down_button_widget.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DropDownButtonWidget<T> extends StatelessWidget {
+  final String label;
   final T? value;
   final String? hint;
   final List<DropdownMenuItem<T>> items;
   final void Function(T?) onChanged;
   const DropDownButtonWidget({
     super.key,
+    required this.label,
     this.value,
     this.hint,
     required this.items,
@@ -15,22 +17,34 @@ class DropDownButtonWidget<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<T>(
-      value: value,
-      isExpanded: true,
-      elevation: 24,
-      iconSize: 35,
-      borderRadius: BorderRadius.circular(10),
-      underline: const SizedBox(),
-      style: Theme.of(context).textTheme.bodyLarge,
-      padding: const EdgeInsets.only(
-        left: 17,
-        right: 32,
-        top: 10,
-        bottom: 10,
-      ),
-      items: items,
-      onChanged: onChanged,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+        DropdownButton<T>(
+          value: value,
+          isExpanded: true,
+          elevation: 24,
+          iconSize: 35,
+          borderRadius: BorderRadius.circular(10),
+          underline: const SizedBox(),
+          style: Theme.of(context).textTheme.bodyLarge,
+          padding: const EdgeInsets.only(
+            left: 17,
+            right: 32,
+            top: 10,
+            bottom: 10,
+          ),
+          items: items,
+          onChanged: onChanged,
+        ),
+      ],
     );
   }
 }
