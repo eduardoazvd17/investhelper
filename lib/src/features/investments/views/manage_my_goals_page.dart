01035fd51990e0704fa37b7aa7354527f6037cc5
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-04-05 15:18:45 -0300

    Creating listing page

--- lib/src/features/investments/views/manage_my_goals_page.dart
+++ lib/src/features/investments/views/manage_my_goals_page.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:investhelper/src/features/investments/controllers/investments_controller.dart';
-
+import '../../../core/widgets/empty_list_widget.dart';
 import '../../../l10n/l10n.dart';
+import '../widgets/goal_card_tile.dart';
 
 class ManageMyGoalsPage extends StatelessWidget {
   static const routeName = "/manageMyGoals";
   final InvestmentsController controller;
   const ManageMyGoalsPage({super.key, required this.controller});
 
+  void _addNewGoal() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.myGoals),
+        actions: [
+          IconButton(
+            onPressed: _addNewGoal,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNewGoal,
+        child: const Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Observer(
+            builder: (_) {
+              return Visibility(
+                visible: controller.goals.isNotEmpty,
+                replacement: Center(
+                  child: EmptyListWidget(
+                    message:
+                        AppLocalizations.of(context)!.emptyManageMyGoalsListing,
+                  ),
+                ),
+                child: ListView(
+                  children: controller.goals.map((goal) {
+                    return GoalCardTile(goal: goal);
+                  }).toList(),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/src/features/investments/views/manage_my_investments_page.dart
+++ lib/src/features/investments/views/manage_my_investments_page.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:investhelper/src/features/investments/controllers/investments_controller.dart';
 
+import '../../../core/widgets/empty_list_widget.dart';
 import '../../../l10n/l10n.dart';
+import '../widgets/investment_tile_widget.dart';
 
 class ManageMyInvestmentsPage extends StatelessWidget {
   static const routeName = "/manageMyInvestments";
   final InvestmentsController controller;
   const ManageMyInvestmentsPage({super.key, required this.controller});
 
+  void _addNewInvestment() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.myInvestments),
+        actions: [
+          IconButton(
+            onPressed: _addNewInvestment,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNewInvestment,
+        child: const Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Observer(
+            builder: (_) {
+              return Visibility(
+                visible: controller.goals.isNotEmpty,
+                replacement: Center(
+                  child: EmptyListWidget(
+                    message: AppLocalizations.of(context)!
+                        .emptyManageMyInvestmentsListing,
+                  ),
+                ),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: controller.investments
+                      .map((e) => InvestmentTileWidget(investment: e))
+                      .toList(),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
