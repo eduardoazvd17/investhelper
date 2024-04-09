eb1fce83aafd3b39ff2cbf62ace0d77f2fd1b609
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-04-09 12:09:25 -0300

    Improving modals

--- lib/src/core/widgets/modal_bottom_sheet_widget.dart
+++ lib/src/core/widgets/modal_bottom_sheet_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ModalBottomSheetWidget extends StatelessWidget {
   final String title;
@@ -19,19 +18,25 @@ class ModalBottomSheetWidget extends StatelessWidget {
     required List<Widget> children,
   }) async {
     return await showModalBottomSheet<T>(
+      elevation: 0,
       context: context,
       isScrollControlled: true,
+      backgroundColor: Colors.transparent,
       builder: (_) {
         return GestureDetector(
           onTap: _hideKeyboard,
           child: Padding(
             padding: EdgeInsets.only(
+              top: MediaQuery.of(context).viewPadding.top,
               bottom: MediaQuery.of(context).viewInsets.bottom,
             ),
-            child: ModalBottomSheetWidget(
-              title: title,
-              actions: actions,
-              children: children,
+            child: Container(
+              color: Theme.of(context).bottomSheetTheme.backgroundColor,
+              child: ModalBottomSheetWidget(
+                title: title,
+                actions: actions,
+                children: children,
+              ),
             ),
           ),
         );
@@ -44,48 +49,52 @@ class ModalBottomSheetWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 5),
-              child: Container(
-                width: 80,
-                height: 3,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey,
-                ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10, bottom: 5),
+            child: Container(
+              width: 80,
+              height: 3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleMedium,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          const Divider(),
+          Flexible(
+            fit: FlexFit.tight,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Column(children: children),
               ),
             ),
+          ),
+          if (actions != null && actions!.isNotEmpty) ...[
             const Divider(),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Column(children: children),
-            ),
-            if (actions != null && actions!.isNotEmpty) ...[
-              const Divider(),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: actions!,
-                ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
               ),
-            ],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: actions!,
+              ),
+            ),
           ],
-        ),
+        ],
       ),
     );
   }
