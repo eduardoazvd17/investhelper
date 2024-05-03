57cc34bf4515eaf202e554772a593198f497fa3a
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-05-03 18:35:58 -0300

    Removing imports

--- lib/src/features/settings/views/settings_page.dart
+++ lib/src/features/settings/views/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
