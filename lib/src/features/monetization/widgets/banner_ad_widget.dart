2265445ba8b3b230effaa9c2061f21ccc5852ba0
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-05-17 11:16:41 -0300

    Fixing load ab problem on web

--- lib/src/features/monetization/widgets/banner_ad_widget.dart
+++ lib/src/features/monetization/widgets/banner_ad_widget.dart
@@ -28,19 +28,17 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
   }
 
   void _loadAd() {
-    if (_bannerAd != null) return;
+    if (_bannerAd != null || kIsWeb) return;
 
     String? adUnitId;
-    if (kIsWeb) {
-      adUnitId = null;
-    } else if (Platform.isAndroid) {
+    if (Platform.isAndroid) {
       adUnitId = 'ca-app-pub-6093298333256656/4449919191';
     } else if (Platform.isIOS) {
       adUnitId = 'ca-app-pub-6093298333256656/6762557078';
     }
 
     /// Testing mode override ID.
-    if (!kIsWeb && kDebugMode) {
+    if (kDebugMode) {
       adUnitId = 'ca-app-pub-3940256099942544/9214589741';
     }
 
